fc866a5838aeaf77059d9810468cc3c26825002e
Kwizera Emmanuel <kwizemmy2004@gmail.com>
2022-07-06 14:27:22 +0200

    add all

--- Frontend/src/views/Vehicles.jsx
+++ Frontend/src/views/Vehicles.jsx
@@ -7,8 +7,11 @@ const Vehicles = () => {
   return (
     <React.Fragment>
       <Header title="Vehicles" />
-      <main>
-
+        <div className="button-container mx-48 my-4 block">
+          <button className="float-right h-10 w-20 bg-[#8288FF] text-white font-serif font-thin">
+             Add
+          </button>
+        </div>
         <div className="container">
           <ul className="responsive-table">
             <li className="table-header">
@@ -21,44 +24,52 @@ const Vehicles = () => {
               <div className="col col-1">Actions</div>
             </li>
             <li className="table-row">
-              <div className="col col-1">N01B1645</div>
-              <div className="col col-1">Toyota</div>
-              <div className="col col-1">Carina</div>
-              <div className="col col-1">1 000$</div>
-              <div className="col col-1">RAA 257C</div>
-              <div className="col col-1">Kalisa</div>
-              <div className="col col-1"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+              <div className="col col-1" data-label="Chesis">N01B1645</div>
+              <div className="col col-1" data-label="Manufacturer">Toyota</div>
+              <div className="col col-1" data-label="Model">Carina</div>
+              <div className="col col-1" data-label="Price">1 000$</div>
+              <div className="col col-1" data-label="PlateNo">RAA 257C</div>
+              <div className="col col-1" data-label="Owner">Kalisa Emmanuel</div>
+              <div className="col col-1 actions" data-label="Actions"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link>&emsp;<Link to="/vehicles" className="text-[#FF0000] font-serif"> Delete</Link></div>
             </li>
             <li className="table-row">
-              <div className="col col-1">N01B1645</div>
-              <div className="col col-1">Toyota</div>
-              <div className="col col-1">Carina</div>
-              <div className="col col-1">1 000$</div>
-              <div className="col col-1">RAA 257C</div>
-              <div className="col col-1">Kalisa</div>
-              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+              <div className="col col-1"data-label="Chesis">N01B1645</div>
+              <div className="col col-1" data-label="Manufacturer">Toyota</div>
+              <div className="col col-1" data-label="Model">Carina</div>
+              <div className="col col-1" data-label="Price">1 000$</div>
+              <div className="col col-1" data-label="PlateNo">RAA 257C</div>
+              <div className="col col-1" data-label="Owner">Kalisa Emmanuel</div>
+              <div className="col col-1 actions" data-label="Actions"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link>  &emsp;  <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
             </li>
             <li className="table-row">
-              <div className="col col-1">N01B1645</div>
-              <div className="col col-1">Toyota</div>
-              <div className="col col-1">Carina</div>
-              <div className="col col-1">1 000$</div>
-              <div className="col col-1">RAA 257C</div>
-              <div className="col col-1">Kalisa</div>
-              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+              <div className="col col-1"data-label="Chesis">N01B1645</div>
+              <div className="col col-1" data-label="Manufacturer">Toyota</div>
+              <div className="col col-1" data-label="Model">Carina</div>
+              <div className="col col-1" data-label="Price">1 000$</div>
+              <div className="col col-1" data-label="PlateNo">RAA 257C</div>
+              <div className="col col-1" data-label="Owner">Kalisa Emmanuel</div>
+              <div className="col col-1 actions" data-label="Actions"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link>  &emsp;  <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
             </li>
             <li className="table-row">
-              <div className="col col-1">N01B1645</div>
-              <div className="col col-1">Toyota</div>
-              <div className="col col-1">Carina</div>
-              <div className="col col-1">1 000$</div>
-              <div className="col col-1">RAA 257C</div>
-              <div className="col col-1">Kalisa</div>
-              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+              <div className="col col-1"data-label="Chesis">N01B1645</div>
+              <div className="col col-1" data-label="Manufacturer">Toyota</div>
+              <div className="col col-1" data-label="Model">Carina</div>
+              <div className="col col-1" data-label="Price">1 000$</div>
+              <div className="col col-1" data-label="PlateNo">RAA 257C</div>
+              <div className="col col-1" data-label="Owner">Kalisa Emmanuel</div>
+              <div className="col col-1 actions" data-label="Actions"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link>  &emsp;  <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+            </li>
+            <li className="table-row">
+              <div className="col col-1"data-label="Chesis">N01B1645</div>
+              <div className="col col-1" data-label="Manufacturer">Toyota</div>
+              <div className="col col-1" data-label="Model">Carina</div>
+              <div className="col col-1" data-label="Price">1 000$</div>
+              <div className="col col-1" data-label="PlateNo">RAA 257C</div>
+              <div className="col col-1" data-label="Owner">Kalisa Emmanuel</div>
+              <div className="col col-1 actions" data-label="Actions"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link>  &emsp;  <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
             </li>
           </ul>
-        </div>          
-      </main>
+        </div> 
     </React.Fragment>
   );
 };

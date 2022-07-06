8269f7f3797386137abdb36a56fa2e95a2bffe6f
Kwizera Emmanuel <kwizemmy2004@gmail.com>
2022-07-06 21:47:20 +0000

    the table

--- Frontend/src/views/Vehicles.jsx
+++ Frontend/src/views/Vehicles.jsx
@@ -1,14 +1,63 @@
 import React from "react";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import "./table.css";
 
 const Vehicles = () => {
   return (
     <React.Fragment>
       <Header title="Vehicles" />
       <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-         Vehicles
-        </div>
+
+        <div className="container">
+          <ul className="responsive-table">
+            <li className="table-header">
+              <div className="col col-1">Chesis</div>
+              <div className="col col-1">Manufacturer</div>
+              <div className="col col-1">Model</div>
+              <div className="col col-1">Price</div>
+              <div className="col col-1">PlateNo</div>
+              <div className="col col-1">Owner</div>
+              <div className="col col-1">Actions</div>
+            </li>
+            <li className="table-row">
+              <div className="col col-1">N01B1645</div>
+              <div className="col col-1">Toyota</div>
+              <div className="col col-1">Carina</div>
+              <div className="col col-1">1 000$</div>
+              <div className="col col-1">RAA 257C</div>
+              <div className="col col-1">Kalisa</div>
+              <div className="col col-1"><Link to="/vehicles" className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+            </li>
+            <li className="table-row">
+              <div className="col col-1">N01B1645</div>
+              <div className="col col-1">Toyota</div>
+              <div className="col col-1">Carina</div>
+              <div className="col col-1">1 000$</div>
+              <div className="col col-1">RAA 257C</div>
+              <div className="col col-1">Kalisa</div>
+              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+            </li>
+            <li className="table-row">
+              <div className="col col-1">N01B1645</div>
+              <div className="col col-1">Toyota</div>
+              <div className="col col-1">Carina</div>
+              <div className="col col-1">1 000$</div>
+              <div className="col col-1">RAA 257C</div>
+              <div className="col col-1">Kalisa</div>
+              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+            </li>
+            <li className="table-row">
+              <div className="col col-1">N01B1645</div>
+              <div className="col col-1">Toyota</div>
+              <div className="col col-1">Carina</div>
+              <div className="col col-1">1 000$</div>
+              <div className="col col-1">RAA 257C</div>
+              <div className="col col-1">Kalisa</div>
+              <div className="col col-1"><Link className="text-[#8288FF] font-serif">Edit</Link> | <Link to="/vehicles" className="text-[#FF0000] font-serif">Delete</Link></div>
+            </li>
+          </ul>
+        </div>          
       </main>
     </React.Fragment>
   );
